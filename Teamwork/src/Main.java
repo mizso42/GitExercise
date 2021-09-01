40749021738a7b6bf15e2bc35956e09f93e40c46
mizso42 <jnzsmkls@gmail.com>
2021-09-01 18:53:09 +0200

    Create random array[20]

--- Teamwork/src/Main.java
+++ Teamwork/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int[] ints = new int[20];
+        Random rand = new Random();
+        for (int i : ints) {
+            i = rand.nextInt(100) + 1;
+        }
     }
 }
