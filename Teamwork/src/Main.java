9eba0aa318a456f1eaf939c87a366406af5b5f6d
BKKrisztina <budavari.kapuszta.krisztina@gmail.com>
2021-09-01 18:56:32 +0200

    Add maxOfArray method

--- Teamwork/src/Main.java
+++ Teamwork/src/Main.java
@@ -2,4 +2,20 @@ public class Main {
     public static void main(String[] args) {
         
     }
-}
+
+        public void maxOfArray(){
+
+            int[] intArray = {33, 2, 0, 34, 12, 110, 2, 14, 22, 80, 30, 5};
+
+            int maxNum = intArray[0];
+
+            for (int i : intArray) {
+                if (i > maxNum)
+                    maxNum = i;
+            }
+
+            System.out.println("Maximum number = " + maxNum);
+
+        }
+    }
+
