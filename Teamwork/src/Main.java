6749c75d77190bd36283b072f6d6600750b504a9
mizso42 <jnzsmkls@gmail.com>
2021-09-01 18:57:18 +0200

    create getMinFromArray method

--- Teamwork/src/Main.java
+++ Teamwork/src/Main.java
@@ -8,4 +8,16 @@ public class Main {
             i = rand.nextInt(100) + 1;
         }
     }
+
+    public static int getMinFromArray(int[] arr) {
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        return min;
+    }
 }
